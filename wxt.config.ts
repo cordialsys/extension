4f5d9c378049db51dce5a46072d577e49bcefa01
Nicolas Stalder <nicolas@cordialsystems.com>
2026-01-28 12:48:57 +0100

    extension: Version submitted as 2nd attempt to get in Chrome Web Store

--- wxt.config.ts
+++ wxt.config.ts
@@ -15,13 +15,15 @@ export default defineConfig({
       128: "/icons/color/128.png",
     },
     host_permissions: [
-      "https://admin.cordialapis.com/*",
+      // "https://admin.cordialapis.com/*",
       // "https://sandbox.cordialapis.com/*",
       // "https://treasury.cordialapis.com/*",
       // "https://clerk.cordial.systems/*",
     ],
     key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAloJLJ8XRkwR+gieAIt1Uc3DfHOZ7nB4LAr74ZkAkM2VBeoTMD8pXJiruJ/FOHNVDmDStBdW00szVFFp2ikySkPFIT/8RXEtVRAuLSLK9q0tVuoTFtFNvaDTMXf5YNV0dOxCv2HHjf/j5xpXNRyAbI2rnkiENJI0cu3zLCPYEufHoT5BhSAeXgtsEtKePHycCfvwLB8hmXZzYXlYX3Yobgssa2KFwL0yx/skVhnxOu3wKLUvzSrCGia6g83TfWjKlJCixMGAPaVdHhQRNTfdh19D2db2mRr/f60XfDCcw+FwVZarM906fd0x+ft1q8JKN32TnIWv0GFUW0Q87iHIeIQIDAQAB",
-    optional_host_permissions: ["https://*/*", "http://*/*"],
+    optional_host_permissions: [
+      // "https://*/*", "http://*/*",
+    ],
     permissions: ["activeTab"],
     web_accessible_resources: [
       {
